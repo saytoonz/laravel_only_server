b85f7bfb0a40f4ed500f169d10dbd56bd3482f4c
Samuel Annin Yeboah <35128520+saytoonz@users.noreply.github.com>
2022-01-02 07:21:03 +0000

    premium

--- app/Http/Controllers/PremiumController.php
+++ app/Http/Controllers/PremiumController.php
@@ -78,8 +78,6 @@ class PremiumController extends Controller
         } catch (\Exception $e) {
             $response["error"] = TRUE;
             $response["msg"] = "An unknown error occurred, please try again.";
-            echo $e;
-            exit();
         }
         return json_encode($response);
     }
