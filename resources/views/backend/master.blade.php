8ee41262c181a6b91a1758e7670230fa5c49de35
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-05-17 15:57:29 +0000

    Update master.blade.php

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -9,8 +9,9 @@
     <title>ONLY Admin</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-    <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
+    <link rel="shortcut icon" href="{{url('images/logo.png')}}" type="image/x-icon">
+    <link rel="apple-touch-icon" href="{{url('images/logo.png')}}">
+    <link rel="shortcut icon" href="{{url('images/logo.png')}}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
@@ -91,7 +92,7 @@
                                     </div>
                                     <div class="stat-content">
                                         <div class="text-left dib">
-                                            <div class="stat-text">$<span class="count">23569</span></div>
+                                            <div class="stat-text"><span class="count">23569</span></div>
                                             <div class="stat-heading">Revenue</div>
                                         </div>
                                     </div>
@@ -104,7 +105,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="stat-widget-five">
-                                    <div class="stat-icon dib flat-color-2">
+                                    <div class="stat-icon dib flat-color-1">
                                         <i class="pe-7s-cart"></i>
                                     </div>
                                     <div class="stat-content">
@@ -122,7 +123,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="stat-widget-five">
-                                    <div class="stat-icon dib flat-color-3">
+                                    <div class="stat-icon dib flat-color-1">
                                         <i class="pe-7s-browser"></i>
                                     </div>
                                     <div class="stat-content">
@@ -140,7 +141,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="stat-widget-five">
-                                    <div class="stat-icon dib flat-color-4">
+                                    <div class="stat-icon dib flat-color-1">
                                         <i class="pe-7s-users"></i>
                                     </div>
                                     <div class="stat-content">
