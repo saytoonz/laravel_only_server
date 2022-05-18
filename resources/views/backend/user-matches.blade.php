472e580ccce5b738bb4b6ecbd4d2d20b746f4b4f
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-05-18 10:13:51 +0000

    Create user-matches.blade.php

---
+++ resources/views/backend/user-matches.blade.php
@@ -0,0 +1,61 @@
+@extends('backend.master')
+@section('content')
+    <!-- Content -->
+    <div class="content">
+        <!-- Animated -->
+        <div class="animated fadeIn">
+            <div class="clearfix"></div>
+            <!-- Orders -->
+            <div class="orders">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="box-title">Matches of {{ $user->first_name }}</h4>
+                            </div>
+                            <div class="card-body--">
+                                <div class="table-stats order-table ov-h">
+                                    <table class="table ">
+                                        <thead>
+                                            <tr>
+                                                <th class="serial">#</th>
+                                                <th class="avatar">User 1</th>
+                                                <th class="avatar">User 2</th>
+                                                <th>Date</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($matches as $key=>$item)
+                                            <tr>
+                                                <td class="serial">{{$key+1}}.</td>
+                                                <td>
+                                                    <a href="{{url("view-user")}}/{{ $item->user1->id}}">
+                                                        <span class="name">{{ $item->user1->first_name}} {{ $item->user1->last_name }}</span>
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    <a href="{{url("view-user")}}/{{ $item->user2->id }}">
+                                                        <span class="name">{{ $item->user2->first_name}} {{ $item->user2->last_name }}</span>
+                                                    </a>
+                                                </td>
+                                                <td><span class="">{{ $item->created_at }}</span></td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    <div class="col-12 d-flex justify-content-end pt-4" class="li: { list-style: none; }">
+                                        {{ $matches->links("pagination::bootstrap-4") }}
+                                    </div>
+                                </div> <!-- /.table-stats -->
+                            </div>
+                        </div> <!-- /.card -->
+                    </div>
+                </div>
+            </div>
+            <!-- /.orders -->
+
+        </div>
+        <!-- .animated -->
+    </div>
+    <!-- /.content -->
+@endsection
