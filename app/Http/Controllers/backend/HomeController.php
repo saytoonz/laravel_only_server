f8d052bec714da79db0d72cac12759fb3b788bdf
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-05-18 10:14:09 +0000

    Create HomeController.php

---
+++ app/Http/Controllers/backend/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('backend.dashboard');
+    }
+
+
+    public function logout()
+    {
+         Auth::logout();
+         return redirect('dashboard');
+   }
+}
