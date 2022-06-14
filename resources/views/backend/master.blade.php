cbc2eaa9ed88b12b3000b20b98fc8203d4d9ce93
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-06-14 14:11:31 +0000

    Update master.blade.php

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -109,6 +109,7 @@
     <!-- /#right-panel -->
 
     <!-- Scripts -->
+
     <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
@@ -132,12 +133,21 @@
     <script src="https://cdn.jsdelivr.net/npm/moment@2.22.2/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.js"></script>
 
+
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ url('js/init/fullcalendar-init.js') }}"></script>
     <script src="{{ url('js/lib/chosen/chosen.jquery.min.js') }}"></script>
 
 
-
+    <script>
+        jQuery(document).ready(function() {
+            jQuery(".standardSelect").chosen({
+                disable_search_threshold: 10,
+                no_results_text: "Oops, nothing found!",
+                width: "100%"
+            });
+        });
+    </script>
 
     <!--Local Stuff-->
     <script>
@@ -338,14 +348,6 @@
             // Bar Chart #flotBarChart End
         });
 
-
-    jQuery(document).ready(function() {
-        jQuery(".standardSelect").chosen({
-            disable_search_threshold: 10,
-            no_results_text: "Oops, nothing found!",
-            width: "100%"
-        });
-    });
 </script>
 </body>
 
