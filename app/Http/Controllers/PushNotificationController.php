b2631fd06246bc346a9784da2757eb10763dc3f7
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-06-15 17:09:25 +0000

    Update PushNotificationController.php

--- app/Http/Controllers/PushNotificationController.php
+++ app/Http/Controllers/PushNotificationController.php
@@ -62,4 +62,41 @@ class PushNotificationController extends Controller
         curl_close($curl);
         // echo $response;
     }
+
+    public function triggerPush($userToken, $title, $message)
+    {
+        $serverToke = SystemApi::find(1);
+        $curl = curl_init();
+        curl_setopt_array(
+            $curl,
+            array(
+                CURLOPT_URL => 'https://fcm.googleapis.com/fcm/send',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => '{
+                    "to" : "'.$userToken.'",
+                    "notification" : {
+                        "body" : "'.$message.'",
+                        "title": "'.$title.'",
+                        "sound": "default"
+                    },
+                    "priority": "high"
+                }',
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json',
+                    'Authorization: Bearer '.$serverToke->api.''
+                ),
+            ),
+        );
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+    }
 }
