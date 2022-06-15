1ceec64eda2bc7fb3297d79ac218fa1167694395
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-06-15 17:09:23 +0000

    Update send-push.blade.php

--- resources/views/backend/notification/send-push.blade.php
+++ resources/views/backend/notification/send-push.blade.php
@@ -6,12 +6,34 @@
             <div class="row">
 
                 <div class="col-xs-6 col-sm-6">
+
+                    @if (Session::has('message'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <button class="close" type="button" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        {{ Session('message') }}
+                    </div>
+                    @endif
+
+
+                    @if (Session::has('error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <button class="close" type="button" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        {{ Session('error') }}
+                    </div>
+                    @endif
+
+                    <form action="{{url('send-push-user')}}" method="POST">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="tbl" value="{{ encrypt('user_push') }}">
+
                     <div class="card">
                         <div class="card-header">
                             <strong class="card-title">Send Push to a user</strong>
                         </div>
                         <div class="card-body">
-                            <select data-placeholder="Choose a user..." class="standardSelect" tabindex="1">
+                            <select data-placeholder="Choose a user..." class="standardSelect" tabindex="1" name="user_id">
                                 <option value="" label="default"></option>
                                 @foreach ($users as $item)
                                     <option value="{{ $item->id }}">
@@ -21,49 +43,79 @@
                             </select>
 
                             <div class="card-body">
-                                <textarea name="message" id="messageUser" cols="30" rows="10" style="width: 100%"></textarea>
+                                <input type="text" id="title" placeholder="Title *" name="title" class="form-control">
+                            </div>
+
+                            <div class="card-body">
+                                <textarea name="notification" cols="30" rows="10" style="width: 100%" placeholder="Message *"></textarea>
                                 <br>
-                               <span style="font-size: 13px;"> Note: All "${username}" would be replaced with the user's first name</span>
+                                <span style="font-size: 13px;"> Note: All "{username}" would be replaced with the user's
+                                    first name</span>
                             </div>
                             <div class="card-body">
-                                <button type="button" class="btn btn-primary">Send</button>
+                                <button type="submit" class="btn btn-primary">Send</button>
                             </div>
                         </div>
                     </div>
+
+                    </form>
                 </div>
 
 
                 <div class="col-xs-6 col-sm-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <strong class="card-title">Send Push to Countries</strong>
-                        </div>
-                        <div class="card-body">
-                            <select data-placeholder="Choose a countries..." multiple class="standardSelect">
-                                <option value="" label="default"></option>
-                                @foreach ($countries as $item)
-                                    <option value="{{ $item->country }}"> {{ $item->country }} ({{ $item->counts }})
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="card-body">
-                            <textarea name="message" id="messageCountry" cols="30" rows="10" style="width: 100%"></textarea>
-                            <br>
-                               <span style="font-size: 13px;"> Note: All "${username}" would be replaced with the user's first name</span>
-                        </div>
-
-                        <div class="card-body">
-                            <button type="button" style="float: right;" class="btn btn-danger">Send</button>
-                        </div>
+                    @if (Session::has('message-2'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <button class="close" type="button" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        {{ Session('message-2') }}
                     </div>
+                    @endif
+
+
+                    @if (Session::has('error-2'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <button class="close" type="button" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        {{ Session('error-2') }}
+                    </div>
+                    @endif
+
+                    <form action="{{url('country-push-user')}}" method="POST">
+                    {{ csrf_field() }}
+                        <input type="hidden" name="tbl" value="{{ encrypt('country_push') }}">
+
+                        <div class="card">
+                            <div class="card-header">
+                                <strong class="card-title">Send Push to Countries</strong>
+                            </div>
+                            <div class="card-body">
+                                <select data-placeholder="Choose a countries..."  class="standardSelect" name="country">
+                                    <option value="" label="default"></option>
+                                    @foreach ($countries as $item)
+                                        <option value="{{ $item->country }}"> {{ $item->country }} ({{ $item->counts }})
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="card-body">
+                                <input type="text" id="title" placeholder="Title *" name="title" class="form-control">
+                            </div>
+
+                            <div class="card-body">
+                                <textarea id="messageCountry" cols="30" rows="10" style="width: 100%" name="notification"></textarea>
+                                <br>
+                                <span style="font-size: 13px;"> Note: All "{username}" would be replaced with the user's first
+                                    name</span>
+                            </div>
+
+                            <div class="card-body">
+                                <button type="submit" style="float: right;" class="btn btn-danger">Send</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
-
-
             </div>
-
-
-        </div><!-- .animated -->
+        </div>
     </div>
 @endsection
