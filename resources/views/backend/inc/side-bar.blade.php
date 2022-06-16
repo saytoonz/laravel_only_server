a7b33434e27f408972099f702cf8c8656c2812e7
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-06-16 11:41:50 +0000

    Update side-bar.blade.php

--- resources/views/backend/inc/side-bar.blade.php
+++ resources/views/backend/inc/side-bar.blade.php
@@ -44,7 +44,7 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false"> <i class="menu-icon fa fa-envelope"></i>Email</a>
                     <ul class="sub-menu children dropdown-menu">
-                        <li><i class="fa ti-email"></i><a href="{{url('new-likes')}}">Send Email</a></li>
+                        <li><i class="fa ti-email"></i><a href="{{url('send-mail')}}">Send Email</a></li>
                     </ul>
                 </li>
 
